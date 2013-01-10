704f9147f65e035caac45132b003107933bec2eb
Yaroslav Kotsur <yarikkotsur@gmail.com>
2013-01-10 17:33:33 +0200

    migrations updated

--- protected/migrations/m130104_150041_create_table_filters_values.php
+++ protected/migrations/m130104_150041_create_table_filters_values.php
@@ -1,21 +1,72 @@
 <?php
+
 class m130104_150041_create_table_filters_values extends CDbMigration {
+
 	public function up() {
 		$this->createTable('filters_values', array(
 			'value_id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
 			'filter_id' => 'INT UNSIGNED NOT NULL',
 			'url' => 'VARCHAR(50) NOT NULL'
-		), 'ENGINE=InnoDB COLLATE=utf8_general_ci');
-		
+				), 'ENGINE=InnoDB COLLATE=utf8_general_ci');
+
 		$this->createIndex('filter_id', 'filters_values', 'filter_id');
 		$this->createIndex('filter_id_url', 'filters_values', 'filter_id,url', true);
-		
+
 		//error find wtf?
 		//$this->addForeignKey('filter_id_fk', 'filters_values', 'filter_id', 'filters', 'filter_id', 'CASCADE', 'CASCADE');
+
+		$this->insert('filters_values', array(
+			'value_id' => 1,
+			'filter_id' => 1,
+			'url' => 'chinese'
+		));
+		
+		$this->insert('filters_values', array(
+			'value_id' => 2,
+			'filter_id' => 1,
+			'url' => 'italian'
+		));
+		
+		$this->insert('filters_values', array(
+			'value_id' => 3,
+			'filter_id' => 1,
+			'url' => 'ukrainian'
+		));
+		
+		$this->insert('filters_values', array(
+			'value_id' => 4,
+			'filter_id' => 1,
+			'url' => 'korean'
+		));
+		
+		$this->insert('filters_values', array(
+			'value_id' => 5,
+			'filter_id' => 2,
+			'url' => 'very-cheap'
+		));
+		
+		$this->insert('filters_values', array(
+			'value_id' => 6,
+			'filter_id' => 2,
+			'url' => 'cheap'
+		));
+		
+		$this->insert('filters_values', array(
+			'value_id' => 7,
+			'filter_id' => 2,
+			'url' => 'middle-price'
+		));
+		
+		$this->insert('filters_values', array(
+			'value_id' => 8,
+			'filter_id' => 2,
+			'url' => 'expensive'
+		));
 	}
 
 	public function down() {
 		$this->dropTable('filters_values');
 		return true;
 	}
+
 }
---
+++ protected/migrations/m130110_102238_create_table_filters_values_desc.php
@@ -0,0 +1,166 @@
+<?php
+
+class m130110_102238_create_table_filters_values_desc extends CDbMigration {
+
+	public function up() {
+		$this->createTable('filters_values_desc', array(
+			'value_id' => 'INT UNSIGNED NOT NULL',
+			'language_id' => 'SMALLINT UNSIGNED NOT NULL',
+			'name' => 'VARCHAR(50) NOT NULL',
+			'description' => 'TEXT'
+				), 'ENGINE=InnoDB COLLATE=utf8_general_ci');
+		
+		$this->createIndex('value_id', 'filters_values_desc', 'value_id');
+		$this->createIndex('language_id', 'filters_values_desc', 'language_id');
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 1,
+			'language_id' => 1,
+			'name' => 'Китайская'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 1,
+			'language_id' => 2,
+			'name' => 'Китайська'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 1,
+			'language_id' => 3,
+			'name' => 'Chinese'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 2,
+			'language_id' => 1,
+			'name' => 'Итальянская'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 2,
+			'language_id' => 2,
+			'name' => 'Італіська'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 2,
+			'language_id' => 3,
+			'name' => 'Italian'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 3,
+			'language_id' => 1,
+			'name' => 'Украинская'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 3,
+			'language_id' => 2,
+			'name' => 'Українська'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 3,
+			'language_id' => 3,
+			'name' => 'Ukrainian'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 4,
+			'language_id' => 1,
+			'name' => 'Корейская'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 4,
+			'language_id' => 2,
+			'name' => 'Корейська'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 4,
+			'language_id' => 3,
+			'name' => 'Korean'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 5,
+			'language_id' => 1,
+			'name' => 'Очень дешево'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 5,
+			'language_id' => 2,
+			'name' => 'Дуже дешево'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 5,
+			'language_id' => 3,
+			'name' => 'Very cheap'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 6,
+			'language_id' => 1,
+			'name' => 'Дешево'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 6,
+			'language_id' => 2,
+			'name' => 'Дешево'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 6,
+			'language_id' => 3,
+			'name' => 'Сheap'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 7,
+			'language_id' => 1,
+			'name' => 'Середняя'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 7,
+			'language_id' => 2,
+			'name' => 'Средняя'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 7,
+			'language_id' => 3,
+			'name' => 'Middle price'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 8,
+			'language_id' => 1,
+			'name' => 'Дорого'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 8,
+			'language_id' => 2,
+			'name' => 'Дорого'
+		));
+		
+		$this->insert('filters_values_desc', array(
+			'value_id' => 8,
+			'language_id' => 3,
+			'name' => 'Expensive'
+		));
+	}
+
+	public function down() {
+		$this->dropTable('filters_values_desc');
+		return true;
+	}
+
+}
