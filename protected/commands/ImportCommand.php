1062a3c9dbca53bfbce3a3bf2fb1bc9cc3fa35d8
tolyamba <mamamusha@mail.ru>
2013-01-10 18:58:39 +0200

    add import kitchens

--- protected/commands/ImportCommand.php
+++ protected/commands/ImportCommand.php
@@ -6,6 +6,7 @@ class ImportCommand extends CConsoleCommand {
 		$q = "SELECT em.member_id, em.facebook_connect_user_id, em.vk_id, em.group_id,  em.password, em.email, em.location, em.bday_d, em.bday_m, em.bday_y, em.avatar_filename, em.photo_filename, em.ip_address, em.join_date, emd.m_field_id_1 AS twitter, emd.m_field_id_3 AS sex, emd.m_field_id_4 AS last_name, emd.m_field_id_6 AS about_tc, emd.m_field_id_7 AS status, emd.m_field_id_9 AS facebook, emd.m_field_id_10 AS city, emd.m_field_id_11 AS vkontakte, emd.m_field_id_12 AS phone, emd.m_field_id_15 AS first_name FROM  `exp_members` AS em JOIN exp_member_data AS emd ON em.member_id=emd.member_id WHERE rest_discounts != 1 ORDER BY em.member_id";
 		$users = Yii::app()->db2->createCommand($q)->queryAll();
 		$i = 0;
+		echo "Begin importing users ...\n";
 		foreach ($users as $user) {
 			if (!$first_name = $user['first_name'])
 				$first_name = '-';
@@ -48,10 +49,32 @@ class ImportCommand extends CConsoleCommand {
 				echo "User #{$i} imported\n";
 			}
 		}
+		echo "END importing users!!!\n";
 	}
 	
 	public function actionKitchens() {
-		echo "Begin importing kitchens ...\n";
+		$q = "SELECT `cat_id`, `cat_name`, `cat_url_title`, `cat_description` FROM `exp_categories` WHERE `group_id` = 9 ORDER BY cat_id";
+		$kitchens = Yii::app()->db2->createCommand($q)->queryAll();
+		$i = 0;
+		echo "Begin importing users ...\n";
+		$command = Yii::app()->db->createCommand();
+		foreach ($kitchens as $kitchen) {
+			$command->insert('filters_values', array(
+				'old_id' => $kitchen['cat_id'],
+				'filter_id' => 1,
+				'url' => $kitchen['cat_url_title']
+			));
+			$id = Yii::app()->db->getLastInsertID();
+			$command->insert('filters_values_desc', array(
+				'value_id' => $id,
+				'language_id' => 1,
+				'name' => $kitchen['cat_name'],
+				'description' => $kitchen['cat_description']
+			));
+			$i++;
+			echo "Kitchen #{$i} imported\n";
+		}
+		echo "END importing kitchens!!!\n";
 	}
 
 }
