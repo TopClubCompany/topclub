bdc71826327f1e322f903f7c5e60ef9b30193f74
tolyamba <mamamusha@mail.ru>
2013-01-11 13:12:24 +0200

    added import places

--- protected/commands/ImportCommand.php
+++ protected/commands/ImportCommand.php
@@ -76,5 +76,52 @@ class ImportCommand extends CConsoleCommand {
 		}
 		echo "END importing kitchens!!!\n";
 	}
+	
+	public function actionPlaces() {
+		$q = "SELECT ecd.entry_id, title, url_title, status, field_id_1 AS name, field_id_2 AS schedule, field_id_3 AS place_desc, field_id_4 AS street, field_id_5 AS street_number, field_id_6 AS place_orientir, field_id_8 AS phone, field_id_9 AS phone2, field_id_11 AS website, field_id_12 AS email, field_id_13 AS cost, field_id_14 AS photo1, field_id_15 AS photo2, field_id_16 AS photo3, field_id_17 AS photo4, field_id_18 AS photo5, field_id_25 AS search_mistakes, field_id_89 AS closed, field_id_96 AS photo6, field_id_97 AS photo7, field_id_98 AS photo8, field_id_99 AS photo9, field_id_100 AS photo10, field_id_108 AS admin_phone,field_id_109 AS order_discount, field_id_110 AS order_discount_banket, field_id_111 AS lat, field_id_112 AS lng FROM exp_channel_data AS ecd JOIN exp_channel_titles AS ect ON ecd.entry_id = ect.entry_id WHERE ecd.channel_id = 1 ORDER BY ecd.entry_id";
+		$places = Yii::app()->db2->createCommand($q)->queryAll();
+		$i = 0;
+		echo "Begin importing places ...\n";
+		$command = Yii::app()->db->createCommand();
+		foreach ($places as $place) {
+			$command->insert('places', array(
+				'place_id' => $place['entry_id'],
+				'title' => $place['title'],
+				'url_title' => $place['url_title'],
+				'status' => $place['status'],
+				'name' => $place['name'],
+				'schedule' => $place['schedule'],
+				'place_desc' => $place['place_desc'],
+				'street' => $place['street'],
+				'street_number' => $place['street_number'],
+				'place_orientir' => $place['place_orientir'],
+				'phone' => $place['phone'],
+				'phone2' => $place['phone2'],
+				'admin_phone' => $place['admin_phone'],
+				'website' => $place['website'],
+				'email' => $place['email'],
+				'cost' => $place['cost'],
+				'lat' => $place['lat'],
+				'lng' => $place['lng'],
+				'order_discount' => $place['order_discount'],
+				'order_discount_banket' => $place['order_discount_banket'],
+				'photo1' => $place['photo1'],
+				'photo2' => $place['photo2'],
+				'photo3' => $place['photo3'],
+				'photo4' => $place['photo4'],
+				'photo5' => $place['photo5'],
+				'photo6' => $place['photo6'],
+				'photo7' => $place['photo7'],
+				'photo8' => $place['photo8'],
+				'photo9' => $place['photo9'],
+				'photo10' => $place['photo10'],
+				'search_mistakes' => $place['search_mistakes'],
+				'closed' => $place['closed']
+			));
+			$i++;
+			echo "Place #{$i} imported\n";
+		}
+		echo "END importing places!!!\n";
+	}
 
 }
