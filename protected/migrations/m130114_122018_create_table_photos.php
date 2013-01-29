a43648fcf708a25b4cedc723c0aa68dc47d5fe36
tolyamba <mamamusha@mail.ru>
2013-01-29 14:33:19 +0200

    table photos fix

--- protected/migrations/m130114_122018_create_table_photos.php
+++ protected/migrations/m130114_122018_create_table_photos.php
@@ -10,7 +10,7 @@ class m130114_122018_create_table_photos extends CDbMigration
 			'user_id' => 'INT UNSIGNED NOT NULL',
 			'title' => 'VARCHAR(100)',
 			'url' => 'VARCHAR(150)',
-			'photoPath' => 'VARCHAR(150)',
+			'filename' => 'VARCHAR(150)',
 			'ip_address' => 'VARCHAR(15)',
 			'status' => 'BOOLEAN NOT NULL DEFAULT 1',
 				), 'ENGINE=InnoDB COLLATE=utf8_general_ci');
